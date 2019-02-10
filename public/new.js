393862a8199280491dafc1a62d5ace4f09d97e38
Riedel <riedelgcamila@gmail.com>
2019-02-10 19:28:55 -0300

    Added fallback image for users without a set pfp

--- public/new.js
+++ public/new.js
@@ -17,74 +17,14 @@ function handleSearch(e){
       
       //console.log(apiString);
 
+   //Start http request to the api
+      const xhr = new XMLHttpRequest();
+      const url = apiString;
+
+      function callOtherDomain() {
 
 
-      //CORS Alternative
-
-      function createCORSRequest(method, url){
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr){
-        xhr.open(method, url, true);
-    } else if (typeof XDomainRequest != "undefined"){
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-    } else {
-        xhr = null;
-    }
-    return xhr;
-}
-
-var request = createCORSRequest("get", apiString);
-if (request){
-    request.onload = function(){
-        //do something with request.responseText
-        var data = JSON.parse(this.response);
-        document.getElementById("results").style.display = "block";
-        data.forEach(person => {
-              //create all the result elements
-              var newTitle= document.createElement('h3');
-              newTitle.textContent = "@" + person.publicId;
-              var name = document.createElement('h5');
-              name.textContent = person.name;
-              var headline = document.createElement('span');
-              headline.textContent = person.professionalHeadline;
-              var picture = document.createElement("img");
-              picture.src = person.picture;
-              var br = document.createElement("br");
-              var weight = document.createElement('span');
-              weight.textContent = "Weight: " + Math.round(person.weight).toString();
-              weight.style.fontWeight="bold";
- 
-
-              //Store them into an array
-              var arrayOfInfo = [newTitle, picture, name, headline, br, weight];
-
-              //Create a paragraph for each result and iterate through the array,
-              //adding the elements
-              var newParagraph = document.createElement('p');
-              for (var i = 0; i < arrayOfInfo.length; i++){
-                newParagraph.appendChild(arrayOfInfo[[i]]);
-              }
-
-              //Wrap results in links to people's profiles
-              var linkToProfile = document.createElement('a');
-              linkToProfile.setAttribute('href', 'https://torre.bio/' + person.publicId);
-              linkToProfile.appendChild(newParagraph);
-              linkToProfile.target = "_blank";
-              document.getElementById("results").appendChild(linkToProfile);
-})
-
-    };
-    request.send();
-}
-
-//end alternative
-   //Start http request to the API
-     // const xhr = new XMLHttpRequest();
-      //const url = apiString;
-
-      /*function callOtherDomain() {
-        if("withCredentials" in xhr) {    
+        if(true) {    
           xhr.open('GET', url, true);
         xhr.onreadystatechange = function() {
               if (this.readyState == 4 && this.status == 200) {
@@ -102,7 +42,12 @@ if (request){
               var headline = document.createElement('span');
               headline.textContent = person.professionalHeadline;
               var picture = document.createElement("img");
-              picture.src = person.picture;
+              if (person.picture ===undefined){
+                picture.src = "images/torre-user.png";
+              } else{
+                picture.src = person.picture;
+              }
+
               var br = document.createElement("br");
               var weight = document.createElement('span');
               weight.textContent = "Weight: " + Math.round(person.weight).toString();
@@ -135,7 +80,7 @@ if (request){
 
         }
       }
-       callOtherDomain();*/
+       callOtherDomain();
     }else{
 
             }
